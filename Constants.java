777efd30815ab70ce7be58beae1e3b5f12caa98a
Md. Nazmul Hasan <nazmul7762@gmail.com>
2023-08-27 11:01:37 +0600

    Step 5 : Constants class added

---
+++ Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+    public static final String LOADING_DATA = "Loading data...";
+
+    public static final String FILE_PATH = "students.txt";
+
+    public static final String LOADED_DATA = "Data Loaded.";
+    public static final String DATE_FORMATE = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String FOUND = "We found it!";
+    public static final String LAST_UPDATE = "\nList last updated on";
+}
